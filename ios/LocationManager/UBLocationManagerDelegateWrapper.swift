958df86d6956f42079a5b2cc9e90f516b383cd0e
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2021-07-26 09:26:15 +0200

    adds iOS location manager

--- ios/LocationManager/UBLocationManagerDelegateWrapper.swift
+++ ios/LocationManager/UBLocationManagerDelegateWrapper.swift
@@ -8,5 +8,16 @@
  *  SPDX-License-Identifier: MPL-2.0
  */
 
+
 import Foundation
-import LayerGpsSharedModule
+
+class UBLocationManagerDelegateWrapper {
+    let usage: UBLocationManager.LocationMonitoringUsage
+
+    weak var delegate: UBLocationManagerDelegate?
+
+    init(_ delegate: UBLocationManagerDelegate, usage: UBLocationManager.LocationMonitoringUsage) {
+        self.delegate = delegate
+        self.usage = usage
+    }
+}
