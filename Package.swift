e44bb912196b134d7996cbca0cb1e25a1ca79314
Bastian Morath <morath@ubique.ch>
2024-03-07 16:17:46 +0100

    Updates package version

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/openmobilemaps/maps-core", from: "2.0.1"),
+        .package(url: "https://github.com/openmobilemaps/maps-core", .upToNextMinor(from: "2.1.0")),
         .package(url: "https://github.com/UbiqueInnovation/ubkit-ios", from: "1.8.1"),
     ],
     targets: [
