04a3245e3b0ab554726e9f0c189756779061187d
Nicolas Märki <maerki@ubique.ch>
2024-11-08 19:26:21 +0100

    log instead of reset
    Signed-off-by: Nicolas Märki <maerki@ubique.ch>

--- ios/UBLocationManager+MCGpsLayer.swift
+++ ios/UBLocationManager+MCGpsLayer.swift
@@ -1,23 +1,21 @@
 import CoreLocation
-import UBLocation
 import UIKit
+import UBLocation
 
 private var binders: [LocationManagerLayerBinder] = []
-private let bindersQueue = DispatchQueue(
-    label: "ch.layergps.ublocationbindersQueue")
+private let bindersQueue = DispatchQueue(label: "ch.layergps.ublocationbindersQueue")
 
-extension UBLocationManager {
-    public func bindTo(layer: MCGpsLayer, canAskForPermission: Bool = false) {
+public extension UBLocationManager {
+    func bindTo(layer: MCGpsLayer, canAskForPermission: Bool = false) {
         let binder = LocationManagerLayerBinder(layer: layer)
 
         bindersQueue.sync {
             binders.append(binder)
         }
 
-        startLocationMonitoring(
-            for: [.location(background: false), .heading(background: false)],
-            delegate: binder,
-            canAskForPermission: canAskForPermission)
+        startLocationMonitoring(for: [.location(background: false), .heading(background: false)],
+                                delegate: binder,
+                                canAskForPermission: canAskForPermission)
 
         layer.setMode(.STANDARD)
     }
@@ -25,7 +23,6 @@ extension UBLocationManager {
 
 public class LocationManagerLayerBinder: NSObject {
     weak var layer: MCGpsLayer?
-    var originalMode: MCGpsMode?
     public init(layer: MCGpsLayer) {
         self.layer = layer
         super.init()
@@ -33,68 +30,48 @@ public class LocationManagerLayerBinder: NSObject {
 }
 
 extension LocationManagerLayerBinder: UBLocationManagerDelegate {
-    public func locationManager(
-        _ manager: UBLocationManager,
-        requiresPermission permission: UBLocationManager.AuthorizationLevel
-    ) {
+    public func locationManager(_ manager: UBLocationManager, requiresPermission permission: UBLocationManager.AuthorizationLevel) {
     }
 
     public var locationManagerFilterAccuracy: CLLocationAccuracy? { nil }
 
-    public func locationManager(
-        _: UBLocationManager,
-        grantedPermission _: UBLocationManager.AuthorizationLevel,
-        accuracy: UBLocationManager.AccuracyLevel
-    ) {
+    public func locationManager(_: UBLocationManager, grantedPermission _: UBLocationManager.AuthorizationLevel, accuracy: UBLocationManager.AccuracyLevel) {
     }
 
-    public func locationManager(permissionDeniedFor manager: UBLocationManager)
-    {
-        originalMode = layer?.mode
-        layer?.setMode(.DISABLED)
+    public func locationManager(permissionDeniedFor manager: UBLocationManager) {
+        let currentLogs = UserDefaults.standard.array(forKey: "ch.layergps.layer.logs") as? [String] ?? []
+        let newLogs = ["\(Date()): PERMISSION DENIED"]  + currentLogs.prefix(10)
+        UserDefaults.standard.set(newLogs, forKey: "ch.layergps.layer.logs")
     }
 
-    public func locationManager(
-        _ manager: UBLocationManager, didUpdateLocations locations: [CLLocation]
-    ) {
+    public func locationManager(_ manager: UBLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
 
         layer?.nativeLayer.setDrawPoint(manager.accuracyLevel == .full)
         layer?.nativeLayer.setDrawHeading(manager.accuracyLevel == .full)
 
-        // position might have been set to disabled after error
-        if let originalMode {
-            layer?.setMode(originalMode)
-            self.originalMode = nil
-        }
-
-        layer?.nativeLayer.updatePosition(
-            .init(
-                systemIdentifier: MCCoordinateSystemIdentifiers.epsg4326(),
-                x: location.coordinate.longitude,
-                y: location.coordinate.latitude,
-                z: location.altitude),
-            horizontalAccuracyM: location.horizontalAccuracy)
+        layer?.nativeLayer.updatePosition(.init(systemIdentifier: MCCoordinateSystemIdentifiers.epsg4326(),
+                                                x: location.coordinate.longitude,
+                                                y: location.coordinate.latitude,
+                                                z: location.altitude), horizontalAccuracyM: location.horizontalAccuracy)
     }
 
-    public func locationManager(_: UBLocationManager, didFailWithError _: Error)
-    {
-        originalMode = layer?.mode
-        layer?.setMode(.DISABLED)
+    public func locationManager(_: UBLocationManager, didFailWithError error: Error) {
+        let currentLogs = UserDefaults.standard.array(forKey: "ch.layergps.layer.logs") as? [String] ?? []
+        let newLogs = ["\(Date()): \(error)"]  + currentLogs.prefix(10)
+        UserDefaults.standard.set(newLogs, forKey: "ch.layergps.layer.logs")
     }
 
-    public func locationManager(
-        _: UBLocationManager, didUpdateHeading newHeading: CLHeading
-    ) {
+    public func locationManager(_: UBLocationManager, didUpdateHeading newHeading: CLHeading) {
         var h = Float(newHeading.trueHeading)
 
         switch UIDevice.current.orientation {
-        case .landscapeRight:
-            h += 90
-        case .landscapeLeft:
-            h -= 90
-        default:
-            break
+            case .landscapeRight:
+                h += 90
+            case .landscapeLeft:
+                h -= 90
+            default:
+                break
         }
 
         layer?.nativeLayer.updateHeading(h)
